9689f23ff99075415ca97ec9cbe18756b640705c
markjnkim <markjnkim@gmail.com>
2020-09-04 06:41:40 -0700

    useHooks

--- src/App.js
+++ src/App.js
@@ -1,56 +1,52 @@
 import React, { useState, useEffect } from 'react';
-// import { useEffect, useState } from 'react'
 import { Table, Button } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThumbsDown, faThumbsUp, faImage, faMoneyCheckAlt, faSearchDollar } from '@fortawesome/free-solid-svg-icons';
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [hasLoaded, setHasLoaded] = useState(false);
   const [invoices, setInvoices] = useState([]);
-}
-   const state = {
-    invoice: [
-      {
-        "id": "100",
-        "Vendor": "Hankook",
-        "Amount": "$18,000",
-        "invoice": "1123",
-        "Date": "08/21/2019"
-      },
-      {
-        "id": "200",
-        "Vendor": "Hankook",
-        "Amount": "$18,000",
-        "invoice": "1123",
-        "Date": "08/21/2019"
-      },
-      {
-        "id": "300",
-        "Vendor": "Hankook",
-        "Amount": "$18,000",
-        "invoice": "1123",
-        "Date": "08/21/2019"
-      }
-    ]
-  }
 
   const remove = (id) => {
     console.log(id);
-    let updateedInvoices = [...this.state.invoices].filter(i => i.id !== id)
+    let updateedInvoices = [...invoices].filter(i => i.id !== id)
     setInvoices(updateedInvoices);
   }
 
-  useEffect( async () => {
-    const response = await fetch("https://r83gdnbzr7.execute-api.us-east-2.amazonaws.com/dev");
-    const body = await response.json();
-    setInvoices(body);
-    console.log(invoices);
-  }, [invoices]);
-   
+  const popInvoices = () => {
     return (
-      <>
+      invoices.map(invoice =>
+        <tr key={invoice.id}>
+          <td>{invoice.Vendor}</td>
+          <td>{invoice.Amount}</td>
+          <td>{invoice.Invoice}</td>
+          <td>{invoice.Date}</td>
+          <td><Button className="btn btn-lg btn-success" onClick={() => remove(invoice.id)} > <FontAwesomeIcon icon={faThumbsUp} /> OK </Button></td>
+          <td><Button className="btn btn-lg btn-danger" onClick={() => remove(invoice.id)} > <FontAwesomeIcon icon={faThumbsDown} /> NOK </Button></td>
+          <td><Button className="btn btn-lg btn-info" onClick={() => remove(invoice.id)} > <FontAwesomeIcon icon={faMoneyCheckAlt} /> 50% </Button></td>
+          <td><Button className="btn btn-lg btn-warning" onClick={() => remove(invoice.id)} ><FontAwesomeIcon icon={faSearchDollar} /> ?? </Button></td>
+          <td><Button className="btn btn-lg btn-info" onClick={() => remove(invoice.id)} > <FontAwesomeIcon icon={faImage} /> Image </Button></td>
+        </tr>)
+    )
+  }
+
+  useEffect( () => {
+      async function fetchData() {
+      const response = await fetch("https://r83gdnbzr7.execute-api.us-east-2.amazonaws.com/dev");
+      const body = await response.json();
+      setInvoices(body);
+      setHasLoaded(true);
+      setIsLoading(false);
+      console.log("BODY", invoices, "hasLoaded", hasLoaded, "isLoading", isLoading);
+      }
+      fetchData();
+  }, []);
+
+  return (
+    <>
       {isLoading && <div>Loading...</div>}
-      <div className="container border border-secondary rouded center">
+      <div className="container mt-4 center">
         <div className="row">
           <div className="col-12">
             <h4>Pending Invoices - The GibiByte Company</h4>
@@ -61,7 +57,7 @@ const App = () => {
             <Table dark responsive striped bordered hover>
               <thead>
                 <tr>
-                  <th >Vendor</th>
+                  <th>Vendor</th>
                   <th>Amount</th>
                   <th>Invoice #</th>
                   <th>Date</th>
@@ -70,26 +66,14 @@ const App = () => {
                 </tr>
               </thead>
               <tbody>
-                {!invoices.length ? <td colSpan="9">All caught up!</td> : 
-      invoices.map(invoice =>
-        <tr key={invoice.id}>
-          <td>{invoice.Vendor}</td>
-          <td>{invoice.Amount}</td>
-          <td>{invoice.invoice}</td>
-          <td>{invoice.Date}</td>
-
-          <td><Button className="btn btn-lg btn-success" onClick={() => this.remove(invoice.id)} > <FontAwesomeIcon icon={faThumbsUp} /> OK </Button></td>
-          <td><Button className="btn btn-lg btn-danger" onClick={() => this.remove(invoice.id)} > <FontAwesomeIcon icon={faThumbsDown} /> NOK </Button></td>
-          <td><Button className="btn btn-lg btn-info" onClick={() => this.remove(invoice.id)} > <FontAwesomeIcon icon={faMoneyCheckAlt} /> 50% </Button></td>
-          <td><Button className="btn btn-lg btn-warning" onClick={() => this.remove(invoice.id)} ><FontAwesomeIcon icon={faSearchDollar} /> ?? </Button></td>
-          <td><Button className="btn btn-lg btn-info" onClick={() => this.remove(invoice.id)} > <FontAwesomeIcon icon={faImage} /> Image </Button></td>
-        </tr>}
+                {invoices.length ? popInvoices() :<td colSpan="9">All caught up!</td>}
               </tbody>
             </Table>
           </div>
         </div>
       </div>
-      </>
-    );
+    </>
+  );
+}
 
 export default App;
